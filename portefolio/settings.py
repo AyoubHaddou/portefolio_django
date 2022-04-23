567eed3d18a649732ec81b54174a9c0777588eaa
ayoub <ayoubhaddou1@gmail.com>
2022-04-23 12:51:59 +0200

    secret user ok

--- portefolio/settings.py
+++ portefolio/settings.py
@@ -144,10 +144,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST_USER  = os.environ.get('SENDGRID_API_NAME')
 # EMAIL_HOST_PASSWORD  =  os.environ.get('SENDGRID_API_KEY')
 
-# autre éssai depuis le ban :
+
+
+from decouple import config
 EMAIL_HOST  =  'smtp.gmail.com' 
-EMAIL_HOST_USER  = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD  =  os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER  = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD  =  config('EMAIL_HOST_PASSWORD')
+
+
 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True 
